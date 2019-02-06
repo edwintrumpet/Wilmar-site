27b9b8499d760e90263b76d773a07cfd62b6799f
Edwin <spark.com.co@gmail.com>
2019-02-06 03:35:35 -0500

    Imagen de portada responsiva y parallax

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('intro', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          transform: 'translateY(-30%)'
+        }),
+        animate('2s ease', style({
+          opacity: 1,
+          transform: 'translateY(0)'
+        }))
+      ])
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  @HostListener('window:scroll', ['$event']) desplazamiento(){
+    let posicion = document.documentElement.scrollTop;
+    let suavizar = posicion / 2;
+
+    document.getElementById('titulo').style.transform = 'translateY(' + -posicion + 'px)';
+    document.getElementById('imagen').style.transform = 'translateY(' + -suavizar + 'px)';
   }
 
 }
